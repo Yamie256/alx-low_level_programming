643130389f863725a9f00d986b0634e4df58751d
yamie256 <yamikanikhalani46@gmail.com>
2022-06-30 17:06:14 -0700

    wow

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*main: Entry
+ * Return: Always 0 (success)
+ * Pintf:with proper grammar,but the outcome is a piece of art
+ */
+int main(void)
+{
+  printf("with proper grammar,but the out come is apiece of art,\n");
+ return (0);
+}
+
