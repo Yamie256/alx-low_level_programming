942be953cec277ac37d1bd69404a575e5a121cbb
yamie256 <yamikanikhalani46@gmail.com>
2022-07-05 07:25:45 -0700

    ykyk

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ * Description-'This program shall print the world,_putchar'.
+ * Return-Always 0(success)
+ */
+int main(void)
+{
+	int _putchar;
+		printf("%d _putchar\n");
+		return (0);
+}
